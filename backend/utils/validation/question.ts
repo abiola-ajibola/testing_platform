ad19e6079b31f9fc735717c7bdd8ab829c5bb6eb
Abiola <ajibolaabiolaahmed@gmail.com>
2025-01-28 09:21:20 +0100

    chore: improve validation for questions

--- backend/utils/validation/question.ts
+++ backend/utils/validation/question.ts
@@ -1,8 +1,30 @@
-import { number, object, string } from "yup";
+import { array, number, object, string } from "yup";
 
 export const createQuestionValidationSchema = object({
   subjectId: string().required("Subject ID is required"),
-  correctOptionId: number().required("Specify correct option ID"),
+  correctOptionId: number().nullable(),
+  explanation: string().nullable(),
+  explanationImageUrl: string().nullable(),
+  options: array(
+    object({
+      text: string().required("Option text is required"),
+      image_url: string().nullable(),
+      correct: string().nullable(),
+    })
+  ).nullable(),
+});
+
+export const getAllValidationSchema = object({
+  page: number().nullable(),
+  perPage: number().nullable(),
+  role: string().nullable(),
+  text: string().nullable(),
+  subjectId: string()
+    .test("", "Please enter a valid subjectId", (value) => {
+      console.log({ value1: value });
+      return !!value && !isNaN(+value);
+    })
+    .nullable(),
   explanation: string().nullable(),
   explanationImageUrl: string().nullable(),
 });
@@ -12,4 +34,11 @@ export const updateQuestionValidationSchema = object({
   correctOptionId: string().nullable(),
   explanation: number().nullable(),
   explanationImageUrl: string().nullable(),
+  options: array(
+    object({
+      text: string().nullable(),
+      image_url: string().nullable(),
+      correct: string().nullable(),
+    })
+  ).nullable(),
 });
