ba9206d918ee58e6181662e0d32f3d6043391718
Abiola <ajibolaabiolaahmed@gmail.com>
2025-01-29 16:54:16 +0100

    improve password input group

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,6 +1,7 @@
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
+import { Eye, EyeOff } from "lucide-react"
 
 const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
   ({ className, type, ...props }, ref) => {
@@ -23,21 +24,43 @@ const InputGroup = React.forwardRef<
   HTMLDivElement,
   React.ComponentProps<"div"> & { helperText?: string; inputProps?: React.ComponentProps<"input">; error?: boolean }
 >(({ className, helperText, inputProps, error, ...props }, ref) => {
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prev) => !prev);
+  };
+
   return (
     <div className={cn("flex flex-col space-y-1", className)} ref={ref} {...props}>
-      <Input
-        {...inputProps}
-        className={cn(inputProps?.className, error ? "border-red-500" : "")}
-      />
+      <div className="relative">
+        <Input
+          {...inputProps}
+          type={inputProps?.type === "password" && showPassword ? "text" : inputProps?.type}
+          className={cn(inputProps?.className, error ? "border-red-500" : "")}
+        />
+        {inputProps?.type === "password" && (
+            <button
+            type="button"
+            onClick={togglePasswordVisibility}
+            className="absolute inset-y-0 right-0 flex items-center pr-3"
+            >
+            {showPassword ? (
+              <Eye className="h-5 w-5" />
+            ) : (
+              <EyeOff className="h-5 w-5" />
+            )}
+            </button>
+        )}
+      </div>
       {helperText && (
         <span className={cn("text-xs", error ? "text-red-500" : "text-muted-foreground")}>
           {helperText}
         </span>
       )}
     </div>
-  )
-})
-InputGroup.displayName = "InputGroup"
+  );
+});
+InputGroup.displayName = "InputGroup";
 
 export { InputGroup }
 
