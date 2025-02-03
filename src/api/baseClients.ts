ec92f3939ab4a2565e3dc6a5f79757af927b0ef1
Abiola <ajibolaabiolaahmed@gmail.com>
2025-02-03 15:38:25 +0100

    Improve base clients for API requests.
    - Add base client for POST requests.
    - Improve base client for GET requests to handle query parameters.

--- src/api/baseClients.ts
+++ src/api/baseClients.ts
@@ -2,15 +2,52 @@ import { AxiosError } from "axios";
 import apiClient from "./client";
 import { toast } from "react-toastify";
 
-export async function simpleGet<T>(url: string) {
+export type ResponseWithPagination<T> = T & {
+  total: number;
+  perPage: number;
+  currentPage: number;
+};
+
+export async function simpleGet<T, U = undefined>(url: string, query?: U) {
   try {
-    const response = await apiClient.get<T>(url);
+    const response = await apiClient.get<T>(url, { params: query });
     return response.data;
   } catch (error) {
-    const er = error as AxiosError<{ count: number }>;
+    const er = error as AxiosError<T>;
     console.error(er);
     toast.error(er.message);
     throw error;
   }
 }
 
+export async function simplePost<DataType, ResponseType>(
+  url: string,
+  data: DataType
+) {
+  try {
+    const response = await apiClient.post<ResponseType>(url, data);
+    toast.success("Created successfully");
+    return response.data;
+  } catch (error) {
+    const er = error as AxiosError<ResponseType>;
+    console.error(er);
+    toast.error(er.message);
+    throw error;
+  }
+}
+
+export async function simplePatch<DataType, ResponseType>(
+  url: string,
+  data: DataType
+) {
+  try {
+    const response = await apiClient.patch<ResponseType>(url, data);
+    toast.success("Updated successfully");
+    return response.data;
+  } catch (error) {
+    const er = error as AxiosError<ResponseType>;
+    console.error(er);
+    toast.error(er.message);
+    throw error;
+  }
+}
