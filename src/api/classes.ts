8e1f44d8cbf895dce9eb0a33b9dcc0cc7845f070
Abiola <ajibolaabiolaahmed@gmail.com>
2025-02-03 15:40:04 +0100

    Add API client for requesting many classes

--- src/api/classes.ts
+++ src/api/classes.ts
@@ -1,5 +1,21 @@
-import { simpleGet } from "./baseClients";
+import { ResponseWithPagination, simpleGet } from "./baseClients";
+
+export interface ICreateClass {
+  name: string;
+  description?: string;
+}
+
+export type ClassResponse = ICreateClass & {
+  id: number;
+  createdAt: string;
+  updatedAt: string;
+};
 
 export const classes = {
   getCount: () => simpleGet<{ count: number }>("/class/count"),
+  getMany: (query?: Partial<ICreateClass>) =>
+    simpleGet<
+      ResponseWithPagination<{ data: ClassResponse[] }>,
+      Partial<ICreateClass>
+    >("/class", query),
 };
